fa090bcf8de1bcb69c296aa84b0fa7e88006da1e
kupendrav <kupendrav99@gmail.com>
2025-03-04 20:55:07 +0530

    Set up Express server with dotenv configuration; add basic route and update main entry point

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,14 @@
-import dotenv from 'dotenv';
-dotenv.config();
 import express from 'express';
+
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+}
+);
+
+export default app;
