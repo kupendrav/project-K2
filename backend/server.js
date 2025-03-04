cbae7061429f4fb4c7cf418e1ba76c71375d769f
kupendrav <kupendrav99@gmail.com>
2025-03-04 21:56:57 +0530

    Fix import statement in server.js to include file extension

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import 'dotenv/config.js';
 import http from 'http';
-import app from './app';
+import app from './app.js';
 
 const port = process.env.PORT || 3000;
 
