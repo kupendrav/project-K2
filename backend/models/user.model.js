d2203012b98009dcd6de969198d46f775b4811fd
kupendrav <kupendrav99@gmail.com>
2025-03-07 17:06:53 +0530

    Refactor user model and routes to enhance user authentication and management; implement login, logout, and profile retrieval functionality

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,6 +1,7 @@
-import mongoose from 'mongoose';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -8,25 +9,34 @@ const userSchema = new mongoose.Schema({
         unique: true,
         trim: true,
         lowercase: true,
-        minLength: [6, 'Email must be at least 6 characters long'],
-        maxLength: [255, 'Email must be at most 255 characters long']
+        minLength: [ 6, 'Email must be at least 6 characters long' ],
+        maxLength: [ 50, 'Email must not be longer than 50 characters' ]
     },
+
     password: {
         type: String,
         select: false,
-    },
-    
-});
+    }
+})
 
-userSchema.statics.haspassword = async function(email, password) {
+
+userSchema.statics.hashPassword = async function (password) {
     return await bcrypt.hash(password, 10);
 }
-userSchema.methods.isValidPassword = async function(password) {
+
+userSchema.methods.isValidPassword = async function (password) {
     return await bcrypt.compare(password, this.password);
 }
-userSchema.methods.genertaeJWT = function() {
-    return jwt.sign({ email: this.email }, process.env.JWT_SECRET);
+
+userSchema.methods.generateJWT = function () {
+    return jwt.sign(
+        { email: this.email },
+        process.env.JWT_SECRET,
+        { expiresIn: '24h' }
+    );
 }
 
-const User = mongoose.model('User', userSchema);
+
+const User = mongoose.model('user', userSchema);
+
 export default User;
